2481fb79eb237a8e2b52230bc015682c5b577898
kalkanlatif <kalkanlatif818@gmail.com>
2026-02-17 05:35:39 -0500

    create a new task groceryTracker to calculate the total amount of the purchased grocery item

---
+++ groceryTracker/groceryTracker.js
@@ -0,0 +1,15 @@
+let amount1;
+let amount2;
+let amount3;
+
+function calculateTotalAmount() {
+
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let total = amount1 + amount2 + amount3;
+  
+    document.getElementById('result').innerText =
+      `The total amount is:$ ${total}`;
+}
